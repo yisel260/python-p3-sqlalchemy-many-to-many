b9d6e098e01af5b09ff62679ccd0c59ec2f059ee
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-03-24 13:53:39 -0400

    completed code along

--- lib/models.py
+++ lib/models.py
@@ -10,6 +10,30 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+game_user = Table(
+    'game_users',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    extend_existing=True,
+)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship('Review', backref=backref('user'))
+    games = relationship('Game', secondary=game_user, back_populates='users')
+
+    # don't forget your __repr__()!
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
+
 class Game(Base):
     __tablename__ = 'games'
 
@@ -20,6 +44,7 @@ class Game(Base):
     price = Column(Integer())
 
     reviews = relationship('Review', backref=backref('game'))
+    users = relationship('User', secondary=game_user, back_populates='games')
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
@@ -32,10 +57,12 @@ class Review(Base):
     id = Column(Integer(), primary_key=True)
     score = Column(Integer())
     comment = Column(String())
-    
+
     game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+
